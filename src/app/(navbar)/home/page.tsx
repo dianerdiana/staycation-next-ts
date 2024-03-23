d5c61c50a6d3936d364b1dd346faba7d1221c18e
dianerdiana <dianerdiana.de@gmail.com>
2024-03-24 00:13:09 +0700

    Create Hero element

--- src/app/(navbar)/home/page.tsx
+++ src/app/(navbar)/home/page.tsx
@@ -1,31 +1,51 @@
+// ** React Import
 import React from 'react';
+
+// ** Next Import
+import Image from 'next/image';
 
 export default function Page() {
   return (
-    <nav className="bg-gray-800 py-4">
-      <div className="container mx-auto px-36 flex justify-between items-center">
-        {/* Brand/logo */}
-        <div>
-          <a href="#" className="text-white font-bold text-xl">
-            Your Brand
-          </a>
+    <main className="max-w-7xl mx-auto px-2 md:px-6 lg:px-10 text-secondary">
+      <div className="container flex flex-col-reverse lg:flex-row py-16">
+        <div className="basis-full lg:basis-6/12">
+          <h1 className="text-[2.50rem] leading-tight font-bold">
+            Forget Busy Work, <br className="hidden lg:inline-block" /> Start Next Vacation
+          </h1>
+          <p className="my-8 font-sm w-96 text-gray-400 font-thin">
+            We provide what you need to enjoy your holiday with family. Time to make another memorable moments.
+          </p>
+          <button className="bg-primary hover:bg-blue-500 delay-75 py-3 px-10 text-white rounded-md shadow-xl">
+            Show Me Now
+          </button>
+          <ul className="mt-20 flex justify-between w-11/12">
+            <li>
+              <Image src="/icons/ic_traveler.svg" alt="ic_traveler" width={32} height={32} />
+              <p className="font-bold mt-2">
+                80,409 <span className="text-gray-400 text-base font-light">travelers</span>
+              </p>
+            </li>
+            <li>
+              <Image src="/icons/ic_treasure.svg" alt="ic_treasure" width={32} height={32} />
+              <p className="font-bold mt-2">
+                862 <span className="text-gray-400 text-base font-light">treasures</span>
+              </p>
+            </li>
+            <li>
+              <Image src="/icons/ic_cities.svg" alt="ic_cities" width={32} height={32} />
+              <p className="font-bold mt-2">
+                1,492 <span className="text-gray-400 text-base font-light">cities</span>
+              </p>
+            </li>
+          </ul>
         </div>
-        {/* Navigation links */}
-        <div className="flex space-x-4">
-          <a href="#" className="text-white hover:text-gray-300">
-            Home
-          </a>
-          <a href="#" className="text-white hover:text-gray-300">
-            Browse By
-          </a>
-          <a href="#" className="text-white hover:text-gray-300">
-            Stories
-          </a>
-          <a href="#" className="text-white hover:text-gray-300">
-            Agent
-          </a>
+        <div className="px-5 pt-5 pb-10 relative flex-shrink-0 basis-full lg:basis-6/12 flex justify-center">
+          <div className="-mt-5 -ms-5 lg:block max-w-full lg:w-[500px] lg:h-[370px]">
+            <img src="/images/img-hero.png" alt="Gambar Utama" className="rounded-tl-[100px] rounded-2xl" />
+          </div>
+          <div className="hidden lg:block absolute w-[480px] h-[410px] border border-gray-400 top-0 right-0 -z-10 mt-5 me-6 rounded-tl-[100px] rounded-2xl"></div>
         </div>
       </div>
-    </nav>
+    </main>
   );
 }
--- src/app/(navbar)/layout.tsx
+++ src/app/(navbar)/layout.tsx
@@ -9,7 +9,7 @@ import { FiAlignJustify, FiX } from 'react-icons/fi';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-export default function NavbarLayout() {
+export default function NavbarLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   // ** Hook
   const currentPath = usePathname();
 
@@ -24,61 +24,68 @@ export default function NavbarLayout() {
   ];
 
   return (
-    <header>
-      <nav className="border border-b">
-        <div className="max-w-7xl mx-auto px-6 md:px-16 lg:px-36 text-secondary">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <Link href="/">
-                  <span className="text-primary font-bold text-xl">Stay</span>
-                  <span className="text-secondary font-bold text-xl">cation</span>
-                </Link>
+    <>
+      <header>
+        <nav className="border border-b">
+          <div className="max-w-7xl mx-auto px-2 md:px-6 lg:px-10 text-secondary">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <Link href="/">
+                    <span className="text-primary font-bold text-xl">Stay</span>
+                    <span className="text-secondary font-bold text-xl">cation.</span>
+                  </Link>
+                </div>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  {navigations.map((nav) => (
+                    <Link
+                      key={nav.id}
+                      href={nav.href}
+                      className={currentPath === nav.href ? 'text-primary px-3 py-2' : 'hover:text-primary px-3 py-2'}
+                    >
+                      {nav.title}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              <div className="-mr-2 flex md:hidden">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md hover:text-white border border-secondary text-secondary hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded={isOpen ? 'true' : 'false'}
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {isOpen ? <FiX size={20} /> : <FiAlignJustify size={20} />}
+                </button>
               </div>
             </div>
-            <div className="hidden md:block">
-              <div className="ml-10 flex items-baseline space-x-4">
+          </div>
+
+          {isOpen && (
+            <div className="md:hidden" id="mobile-menu">
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
                 {navigations.map((nav) => (
-                  <Link key={nav.id} href={nav.href} className="hover:text-primary px-3 py-2 text-sm font-medium">
-                    <span className={currentPath === nav.href ? 'text-primary' : 'text-secondary'}>{nav.title}</span>
+                  <Link
+                    key={nav.id}
+                    href={nav.href}
+                    className={`
+                  hover:bg-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium
+                  ${currentPath === nav.href ? 'text-white bg-secondary' : 'text-secondary'}
+                  `}
+                  >
+                    {nav.title}
                   </Link>
                 ))}
               </div>
             </div>
-            <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="inline-flex items-center justify-center p-2 rounded-md hover:text-white border border-secondary text-secondary hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded={isOpen ? 'true' : 'false'}
-              >
-                <span className="sr-only">Open main menu</span>
-                {isOpen ? <FiX size={20} /> : <FiAlignJustify size={20} />}
-              </button>
-            </div>
-          </div>
-        </div>
-
-        {isOpen && (
-          <div className="md:hidden" id="mobile-menu">
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              {navigations.map((nav) => (
-                <Link
-                  key={nav.id}
-                  href={nav.href}
-                  className={`
-                  hover:bg-secondary hover:text-white block px-3 py-2 rounded-md text-base font-medium
-                  ${currentPath === nav.href ? 'text-white bg-secondary' : 'text-secondary'}
-                  `}
-                >
-                  {nav.title}
-                </Link>
-              ))}
-            </div>
-          </div>
-        )}
-      </nav>
-    </header>
+          )}
+        </nav>
+      </header>
+      {children}
+    </>
   );
 }
